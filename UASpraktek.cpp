9c0dcb8fd6117acc3aa0f2c26c0fc8286ca92de1
Alhafiz63 <hafizbanjar.jan05@gmail.com>
2024-07-13 20:39:29 +0800

    Add:fungsi hitungNilaiAkhir

--- UASpraktek.cpp
+++ UASpraktek.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+
+double hitungNilaiAkhir(double nilaiTugas, double nilaiUTS, double nilaiUAS) {
+   
+    double bobotTugas = 0.3;
+    double bobotUTS = 0.3;
+    double bobotUAS = 0.4;
+
+    double nilaiAkhir = (nilaiTugas * bobotTugas) + (nilaiUTS * bobotUTS) + (nilaiUAS * bobotUAS);
+    return nilaiAkhir;
+}
